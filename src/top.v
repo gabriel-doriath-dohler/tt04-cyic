ddd29a9d0ac8c628c0692fd009b4556f9d62c73a
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-08-05 12:51:41 +0000

    Fix syntax

--- src/top.v
+++ src/top.v
@@ -13,7 +13,7 @@ module tt_um_sup3legacy_trng (
   wire random_bit;
 
   ring_oscillator oscillator(
-    .entropy_bit (random_bit);
+    .entropy_bit (random_bit),
   );
 
   assign uo_out = {7'b0000000, random_bit};
