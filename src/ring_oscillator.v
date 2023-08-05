949275c61d0a2fbcc45d912bc8365f5f7b25a381
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-08-05 15:25:16 +0000

    Revert back to assign-type ring oscillator

--- src/ring_oscillator.v
+++ src/ring_oscillator.v
@@ -9,14 +9,21 @@ module ring_oscillator (
   wire n5;
   wire n6;
 
-  // TODO: Add emulate random for the simulation
-  sky130_fd_sc_hd__inv_2 nand0 (.Y(n1), .A(n0));
-  sky130_fd_sc_hd__inv_2 nand1 (.Y(n2), .A(n1));
-  sky130_fd_sc_hd__inv_2 nand2 (.Y(n3), .A(n2));
-  sky130_fd_sc_hd__inv_2 nand3 (.Y(n4), .A(n3));
-  sky130_fd_sc_hd__inv_2 nand4 (.Y(n5), .A(n4));
-  sky130_fd_sc_hd__inv_2 nand5 (.Y(n6), .A(n5));
-  sky130_fd_sc_hd__inv_2 nand6 (.Y(n0), .A(n6));
+  assign n1 = !n0;
+  assign n2 = !n1;
+  assign n3 = !n2;
+  assign n4 = !n3;
+  assign n5 = !n4;
+  assign n6 = !n5;
+  assign n0 = !n6;
+
+  //sky130_fd_sc_hd__inv_2 nand0 (.Y(n1), .A(n0));
+  //sky130_fd_sc_hd__inv_2 nand1 (.Y(n2), .A(n1));
+  //sky130_fd_sc_hd__inv_2 nand2 (.Y(n3), .A(n2));
+  //sky130_fd_sc_hd__inv_2 nand3 (.Y(n4), .A(n3));
+  //sky130_fd_sc_hd__inv_2 nand4 (.Y(n5), .A(n4));
+  //sky130_fd_sc_hd__inv_2 nand5 (.Y(n6), .A(n5));
+  //sky130_fd_sc_hd__inv_2 nand6 (.Y(n0), .A(n6));
 
   assign entropy_bit = n0;
 endmodule
