c8edfa5c1bb19207b90336bb3ae55541703f1df8
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-08-05 12:42:25 +0000

    Add ring oscillator module

---
+++ src/ring_oscillator.v
@@ -0,0 +1,20 @@
+module ring_oscillator (
+    output bit
+);
+  wire n0;
+  wire n1;
+  wire n2;
+  wire n0;
+  wire n4;
+  wire n5;
+  wire n6;
+  assign n1 = ~n0;
+  assign n2 = ~n1;
+  assign n3 = ~n2;
+  assign n4 = ~n3;
+  assign n5 = ~n4;
+  assign n6 = ~n5;
+  assign n0 = ~n6;
+
+  assign bit = n0;
+endmodule
