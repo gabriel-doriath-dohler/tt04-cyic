c9f02a35b6ca048cfb2947b8ac6049e3efb61c4a
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-08-05 16:45:40 +0000

    Try without the linter

--- src/config.tcl
+++ src/config.tcl
@@ -11,6 +11,9 @@ set script_dir [file dirname [file normalize [info script]]]
 # - the list of source files
 source $::env(DESIGN_DIR)/user_config.tcl
 
+# WARN: Disabled linter
+set ::env(RUN_LINTER) 0
+
 # save some time
 set ::env(RUN_KLAYOUT_XOR) 0
 set ::env(RUN_KLAYOUT_DRC) 0
