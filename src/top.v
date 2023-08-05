3c28a21ccfd57bdd45b1fe11830defbc3c0e454d
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-08-05 15:15:10 +0000

    Fix oe port: output

--- src/top.v
+++ src/top.v
@@ -4,15 +4,15 @@ module tt_um_sup3legacy_trng (
   input rst_n,
   input [7:0] ui_in,
   input [7:0] uio_in,
-  input [7:0] uio_oe,
+  output [7:0] uio_oe,
   output [7:0] uio_out,
   output [7:0] uo_out
 );
   wire random_bit;
 
-  //ring_oscillator oscillator(
-  //  .entropy_bit (random_bit),
-  //);
+  ring_oscillator oscillator(
+    .entropy_bit (random_bit),
+  );
 
   assign random_bit = 0;
   assign uo_out = {7'b0000000, random_bit};
