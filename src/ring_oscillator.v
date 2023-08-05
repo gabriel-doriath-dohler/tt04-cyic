eace22ba3378ca7e917654a632667a42e3ea75be
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-08-05 13:26:44 +0000

    Nand -> inv

--- src/ring_oscillator.v
+++ src/ring_oscillator.v
@@ -10,13 +10,13 @@ module ring_oscillator (
   wire n6;
 
   // TODO: Add emulate random for the simulation
-  sky130_fd_sc_hd__nand4_4 nand0 (.Y(n1), .A(n0), .B(n0), .C(n0), .D(n0));
-  sky130_fd_sc_hd__nand4_4 nand1 (.Y(n2), .A(n1), .B(n1), .C(n1), .D(n1));
-  sky130_fd_sc_hd__nand4_4 nand2 (.Y(n3), .A(n2), .B(n2), .C(n2), .D(n2));
-  sky130_fd_sc_hd__nand4_4 nand3 (.Y(n4), .A(n3), .B(n3), .C(n3), .D(n3));
-  sky130_fd_sc_hd__nand4_4 nand4 (.Y(n5), .A(n4), .B(n4), .C(n4), .D(n4));
-  sky130_fd_sc_hd__nand4_4 nand5 (.Y(n6), .A(n5), .B(n5), .C(n5), .D(n5));
-  sky130_fd_sc_hd__nand4_4 nand6 (.Y(n0), .A(n6), .B(n6), .C(n6), .D(n6));
+  sky130_fd_sc_hd__inv_2 nand0 (.Y(n1), .A(n0));
+  sky130_fd_sc_hd__inv_2 nand1 (.Y(n2), .A(n1));
+  sky130_fd_sc_hd__inv_2 nand2 (.Y(n3), .A(n2));
+  sky130_fd_sc_hd__inv_2 nand3 (.Y(n4), .A(n3));
+  sky130_fd_sc_hd__inv_2 nand4 (.Y(n5), .A(n4));
+  sky130_fd_sc_hd__inv_2 nand5 (.Y(n6), .A(n5));
+  sky130_fd_sc_hd__inv_2 nand6 (.Y(n0), .A(n6));
 
   assign entropy_bit = n0;
 endmodule
