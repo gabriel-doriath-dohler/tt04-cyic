5457e81f58916def0176d93aa0f24e850b7bfc2b
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2023-08-05 13:34:31 +0000

    Test without ring oscillator

--- src/top.v
+++ src/top.v
@@ -12,10 +12,11 @@ module tt_um_sup3legacy_trng (
   );
   wire random_bit;
 
-  ring_oscillator oscillator(
-    .entropy_bit (random_bit),
-  );
+  //ring_oscillator oscillator(
+  //  .entropy_bit (random_bit),
+  //);
 
+  assign rnadom_bit = 0;
   assign uo_out = {7'b0000000, random_bit};
   assign uio_oe = 0;
   assign uio_out = 0;
